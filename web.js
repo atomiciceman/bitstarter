ea374128ac7bfa57c2e4a0bb15568cb86dd59652
Ubuntu <ubuntu@ip-172-31-32-207.us-west-2.compute.internal>
2013-07-05 22:29:13 +0000

    changed text to index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello AtomicIceman!');
+  var buffer = new Buffer(fs.readFileSync('./index.html'));
+  var content = buffer.toString('utf-8');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
